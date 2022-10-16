be84711d2c0c6dc5b734d9b96caec1661fe7109a
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-10-16 05:16:27 +0100

    Update Home.jsx

--- Desktop/crypt/src/components/Home.jsx
+++ Desktop/crypt/src/components/Home.jsx
@@ -3,7 +3,7 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
-import { useGetCryptosQuery } from '../components/API/CryptApi';
+import { useGetPostsQuery } from '../components/API/CryptApi';
 import Crypto from './Crypto';
 import News from './News';
 
